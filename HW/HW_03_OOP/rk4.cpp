eb1034e33621f1261a83ad33de6c3b52e2cb56ee
CocoUrbina <edurbinaq@gmail.com>
2025-10-26 23:47:49 -0600

    rk4 updated

---
+++ HW/HW_03_OOP/rk4.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+// Step RK4
+double step_rk4(double (func*)(double, double), double x, double y, double h){
+  double k_1 = func(x, y);
+  double k_2 = func(x + h/2.0, y + h*k_1/2.0);
+  double k_3 = func(x + h/2.0, y + h*k_2/2.0);
+  double k_4 = func(x + h, y + h*k_3);
+
+  return y + (h/6.0)*(k_1 + 2*k_2 + 2*k_3 + k_4);
+}
+
+// Integral
+double int_rk4(double (func*)(double, double), double x_0, double x_1, double y_0, int N){
+  double h = (x_1 - x_0) / N;
+  double x = x_0;
+  double y = y_0;
+
+  for (int i = 0; i < N; ++i){
+    y = step_rk4(func, x, y, h);
+    x = x + h;
+  }
+
+  return y;
+}
+
+// EDO
+double func(double x, double y){
+    return 2*(1 - y) - std::exp(-4*x);
+}
+
+int main() {
+    double x_0 = 0.0; // lower limit
+    double x_1 = 2.0; // upper limit
+    double y_0 = 1.0; // Initial condition
+    int N = 100;     // Steps
+
+    double y_final = int_rk4(f, x_0, x_1, y_0, N);
+
+    std::cout << "y(" << x_1 << ") ≈ " << y_final << "\n";
+
+    return 0;
+}
+
+
+
+
