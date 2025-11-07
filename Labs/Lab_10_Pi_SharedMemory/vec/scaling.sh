c55ac46e6196de38dba9d78de7905a978bc73d6f
CocoUrbina <edurbinaq@gmail.com>
2025-11-07 14:55:41 -0600

    lab10 updated

--- Labs/Lab_10_Pi_SharedMemory/vec/scaling.sh
+++ Labs/Lab_10_Pi_SharedMemory/vec/scaling.sh
@@ -5,12 +5,23 @@ echo "Compilando maxThreads.cpp..."
 g++ maxThreads.cpp -o maxThreads.x -fopenmp
 
 if [ $? -ne 0 ]; then
+  # $? : Codigo de salida del ultimo comando ejecutado
+  # -ne 0 : not equal 0
+  # Si un codigo de salida es cero, fue exitoso. Si no es cero, fue error
+  # Entonces esa condicion verifica si fallo o fue exitoso
     echo "Error al compilar maxThreads.cpp"
-    exit 1
+    exit 1 # termina el script con codigo de salida 1 (error porque no es cero)
 fi
 
 echo "Ejecutando maxThreads.x..."
 MAX_THREADS=$(./maxThreads.x | awk '{print $3}')
+# $(...) ejecuta lo que esta dentro de los () y guarda la salida como texto
+# el pipe | redirige la salida al siguiente comando
+# awk procesa texto por columnas
+# {print $3}, la tercera columna, ya que la salida de maxThreads.x es : "Maximo posible: <num>"
+# col1:"Maximo"
+# col2:" posible:"
+# col3: <num>
 echo "Máximo número de hilos (OpenMP): $MAX_THREADS"
 
 #================= Compilar el cpp ======================
@@ -25,7 +36,13 @@ echo "Compilación exitosa: vector.x generado"
 
 #============= Blanquear el archivo ====================
 if [ -f speedupVector.dat ]; then
-    echo "Eliminando archivo previo scaling.dat..."
+  # -f : file
+  # -d : directorio
+  # -e : cualquier tipo
+  # -r : readable
+  # -w : writable
+  # -x : executable
+    echo "Eliminando archivo previo speedupVector.dat..."
     rm speedupVector.dat
 fi
 
