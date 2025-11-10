bc0524ab132be229d3d358006d5eaf31c69dd80a
CocoUrbina <edurbinaq@gmail.com>
2025-11-09 18:31:10 -0600

    HW04 part I

--- HW/HW_04_HPC/fourier.cpp
+++ HW/HW_04_HPC/fourier.cpp
@@ -5,27 +5,18 @@
 #include <omp.h>
 #include <sys/time.h>
 
-double seconds() {
-  struct timeval tmp;
-  double sec;
-  gettimeofday( &tmp, (struct timezone *)0 );
-  sec = tmp.tv_sec + ((double)tmp.tv_usec)/1000000.0;
-  
-  return sec;
-}
-
 // Note que la rutina acepta referencia a vectores con componentes complejos
 void dft(std::vector< std::complex<double> > &input, std::vector< std::complex<double> > &output){
   
   // Tamaño del vector de entrada
   int N = input.size();
-
+  
   int num_procs = 0;
-
-  std::cout << "Default Threads: " << omp_get_max_threads() << std::endl;
-
   
   double pi = std::acos(-1.0);
+
+  double start = omp_get_wtime();
+  
   // Note que std::complex acepta números complejos usando la notación {real, imaginario}
   // donde la primera entrada es la parte real y la segunda entrada es la parte imaginaria
   // Rutina principal
@@ -46,7 +37,9 @@ void dft(std::vector< std::complex<double> > &input, std::vector< std::complex<d
     }
   } // fin region paralelo
 
-  std::cout << "Used Threads: " << num_procs << std::endl;
+  double end = omp_get_wtime();
+
+ std::cout << num_procs <<" " << end - start << std::endl;
 
 }
 
@@ -66,17 +59,8 @@ int main() {
     in[n] = std::sin(2.0 * x_vals[n]);
   }
 
-  // La función omp_get_wtime() funciona de la misma forma que la función seconds() 
-  // vista en clase.
-  //double start = omp_get_wtime();
-  double time_1 = seconds();
-  dft(in, out);
-  double time_2 = seconds();
-  //double end = omp_get_wtime();
-  
+  dft(in, out); 
   // Puede visualizar la transformada mediante los elementos del vector "out"
-
-  std::cout << "Tiempo de la DFT: " << (time_2 - time_1) << " seconds." << std::endl;
   
   return 0;
 }
---
+++ HW/HW_04_HPC/scalingFourier.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+#============= Obtener numero de hilos maximo ==========
+echo "Compilando maxThreads.cpp..."
+g++ maxThreads.cpp -o maxThreads.x -fopenmp
+
+if [ $? -ne 0 ]; then
+  # $? : Codigo de salida del ultimo comando ejecutado
+  # -ne 0 : not equal 0
+  # Si un codigo de salida es cero, fue exitoso. Si no es cero, fue error
+  # Entonces esa condicion verifica si fallo o fue exitoso
+    echo "Error al compilar maxThreads.cpp"
+    exit 1 # termina el script con codigo de salida 1 (error porque no es cero)
+fi
+
+echo "Ejecutando maxThreads.x..."
+MAX_THREADS=$(./maxThreads.x | awk '{print $3}')
+# $(...) ejecuta lo que esta dentro de los () y guarda la salida como texto
+# el pipe | redirige la salida al siguiente comando
+# awk procesa texto por columnas
+# {print $3}, la tercera columna, ya que la salida de maxThreads.x es : "Maximo posible: <num>"
+# col1:"Maximo"
+# col2:" posible:"
+# col3: <num>
+echo "Máximo número de hilos (OpenMP): $MAX_THREADS"
+
+#================= Compilar el cpp ======================
+echo "Compilando fourier.cpp..."
+g++ fourier.cpp -o fourier.x -fopenmp
+
+if [ $? -ne 0 ]; then
+    echo "Error al compilar fourier.cpp"
+    exit 1
+fi
+echo "Compilación exitosa: fourier.x generado"
+
+#============= Remover el archivo ====================
+if [ -f scaling.dat ]; then
+  # -f : file
+  # -d : directorio
+  # -e : cualquier tipo
+  # -r : readable
+  # -w : writable
+  # -x : executable
+    echo "Eliminando archivo previo scaling.dat..."
+    rm scaling.dat
+fi
+
+#============ Ejecutar el ejecutable! ================
+echo "Ejecutando pruebas de escalabilidad..."
+for (( t=1; t<=${MAX_THREADS}; t++ )); do
+    echo "Ejecutando con $t hilos..."
+    OMP_NUM_THREADS=$t ./fourier.x >> scaling.dat
+done
+
+#=================== Speedup =========================
+T1=$(head -n 1 scaling.dat | awk '{print $2}')
+awk -v T1="$T1" '{printf "%d %.6f\n", $1, T1/$2}' scaling.dat > speedupFourier.dat
+# head -n 1 scaling.dat : toma la primera fila de scaling.dat
+# | el pipe pasa ese outcome al siguiente comando
+# awk '{print $2}' : awk explora por tablulaciones. En este caso, toma col2
+# Con eso, ya tomamos el tiempo para thread = 1 y se guarda en T1
+#
+# Con -v pasamos la variable de shell T1 al programa awk como la variable awk T1. De ese modo awk puede usar el valor de T1 dentro de su programa
+
+# '{printf "%d %.6f\n", $1, T1/$2}'
+# por cada línea de scaling.dat, awk hace:
+# $1 es la primera columna (número de hilos).
+# $2 es la segunda columna (el tiempo T_p).
+# T1/$2 calcula el speedup.
+# printf "%d %.6f\n", $1, T1/$2 imprime dos columnas: primera como entero (%d) y segunda como float con 6 decimales (%.6f), y un salto de línea.
+
+# scaling.dat > speedupFourier.dat
+# awk lee scaling.dat y la salida formateada se redirige a speedupFourier.dat.
+
+#================== Graficar ==========================
+if [ -f fig.plt ]; then
+    echo "Generando figura con gnuplot..."
+    gnuplot fig.plt
+    echo "Figura generada correctamente."
+else
+    echo "No se encontró el archivo fig.plt. Saltando paso de graficado."
+fi
+
+echo "Rutina de escalabilidad completada."
