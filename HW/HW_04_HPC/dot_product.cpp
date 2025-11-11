1ed85e71c1c9ae2c2ad2dd772b289208fdb614ef
CocoUrbina <edurbinaq@gmail.com>
2025-11-11 03:04:45 -0600

    dot_product.cpp

--- HW/HW_04_HPC/dot_product.cpp
+++ HW/HW_04_HPC/dot_product.cpp
@@ -27,6 +27,8 @@ int main(){
 
   // 4-Para todos los procesos, utilice el método resize() para asignar suficiente memoria para nlocal elementos para los vectores local_a y local_b. Defina nlocal como lo hicimos en clase. Puede asumir que el número de elementos N es un múltiplo del número de procesos size (i.e., no hay rest) (0.5%)
   nlocal = N / size;
+  local_a.resize(nlocal);
+  local_b.resize(nlocal);
 
   // 5-En el proceso 0, asigne distintos valores a las entradas de los vectores a y b. Asigne estos valores de manera tal que a = b = [1, 2, 3, · · · N ] (0.25%)
   if(rank == 0){
@@ -58,11 +60,13 @@ int main(){
       MPI_COMM_WORLD);
 
   // 7-Calcule el producto interno de cada sección de tamaño nlocal en cada proceso (0.25%)
+  double local_dot = 0.0;
   for(int i = 0; i < nlocal; i++){
     local_dot += local_a[i] * local_b[i];
   }
 
   // 8-Utilice MPI_Reduce para calcular el producto interno total con base en los productos internos de cada proceso (0.5%)
+  double global_dot = 0.0;
   MPI_Reduce(&local_dot, &global_dot, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
   // 9-Imprima el resultado usando el proceso 0 (0.25%)
